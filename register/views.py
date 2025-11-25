c964384f1c1644c34ca49923954eef03634b4259
Welile Nkomo <welilelyvia@gmail.com>
2025-11-25 12:10:53 +0200

    feat: add login template and update views + styles

--- register/views.py
+++ register/views.py
@@ -1,19 +1,20 @@
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import IdentityVerificationForm
-from .forms import IdentityVerificationForm
+from django.contrib.auth import authenticate, login
 
 def verify_identity(request):
     if request.method == "POST":
         form = IdentityVerificationForm(request.POST)
         if form.is_valid():
-            form.save()  # ✅ saves to database
-            return render(request, "register/success.html", {"form": form})
+            identity = form.save()  # save and get the instance
+            return render(request, "register/success.html", {
+                "full_names": identity.full_names,
+                "surname": identity.surname,
+                "id_number": identity.id_number,
+                "cellphone": identity.cellphone,
+            })
     else:
-        identity = form.save()
-        return render(request, "register/success.html", {
-              "full_names": identity.full_names,
-              "surname": identity.surname,
-              "id_number": identity.id_number,
-              "cellphone": identity.cellphone,
-})
+        form = IdentityVerificationForm()  # empty form for GET
+
+    return render(request, "register/verify.html", {"form": form})
+
